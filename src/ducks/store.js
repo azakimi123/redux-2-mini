df29052a2d919714b864f5c88edde313a3b4b510
azakimi123 <aaron.zakimi@gmail.com>
2020-07-08 08:55:15 -0600

    step 4 completed

---
+++ src/ducks/store.js
@@ -0,0 +1,6 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise-middleware'
+import hackerNewsReducer from './hackerNewsReducer';
+
+
+export default createStore(hackerNewsReducer, applyMiddleware(promiseMiddleware));
